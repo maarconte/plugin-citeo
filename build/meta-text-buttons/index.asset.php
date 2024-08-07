695455b4e5c2df7b6e380173ad338124f667b242
Mathilde Arconte <signup@marconte.fr>
2024-08-07 21:27:01 +0200

    + prettier

--- build/meta-text-buttons/index.asset.php
+++ build/meta-text-buttons/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'b6c56a94de56c11f711b');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'a3d5f47c6848773a5f30');
